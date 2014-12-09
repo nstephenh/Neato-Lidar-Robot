027220d977dc92c9747316c47051dede8c8cbfda
Noah Stephen Haskell <nstephenh@gmail.com>
2014-12-09 10:05:31 -0500

    Changed LIDAR.py to be able to recieve serial data and print it to the screen.

--- Code/LIDAR.py
+++ Code/LIDAR.py
@@ -5,7 +5,7 @@ print "Lidar Sensor Program by Noah Haskell"
 port = "/dev/ttyACM0"
 baud = 115200
 ser = serial.Serial(port, baud)
-
-while True:
+while t>10000:
 	data = (ser.read())
 	print ord(data)
+	t= t+1 
