4119ae389845c5e9f85fc51da8745aa7d247b15a
Noah Stephen Haskell <nstephenh@gmail.com>
2014-12-09 14:59:23 -0500

    Started on processing data in LIDAR.py

--- Code/LIDAR.py
+++ Code/LIDAR.py
@@ -5,7 +5,15 @@ print "Lidar Sensor Program by Noah Haskell"
 port = "/dev/ttyACM0"
 baud = 115200
 ser = serial.Serial(port, baud)
-while t>10000:
-	data = (ser.read())
-	print ord(data)
-	t= t+1 
+
+packetposition = 0
+while data:
+	data = ord((ser.read()))
+	if data == 250:
+		print("start of packet")
+		packetposition = 1
+	if packetposition == 1
+		multiplier = data
+		packetposition = 2
+	if packetposition = 3
+	
